e0d15c9bd63aa3cd2e87ff8d0b6901be02207ccf
2013o <2013o@DESKTOP-KGS0845>
2025-03-24 22:12:20 +0700

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Lab3VIchmat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.Graph = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.Lagranshz = new System.Windows.Forms.Button();
+            this.xTextBox = new System.Windows.Forms.TextBox();
+            this.yTextBox = new System.Windows.Forms.TextBox();
+            this.Neuton = new System.Windows.Forms.Button();
+            this.Save_Button = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Graph)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Graph
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.Graph.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.Graph.Legends.Add(legend3);
+            this.Graph.Location = new System.Drawing.Point(27, 22);
+            this.Graph.Name = "Graph";
+            series5.ChartArea = "ChartArea1";
+            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series5.Legend = "Legend1";
+            series5.Name = "Series1";
+            series6.ChartArea = "ChartArea1";
+            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series6.Legend = "Legend1";
+            series6.Name = "Series2";
+            this.Graph.Series.Add(series5);
+            this.Graph.Series.Add(series6);
+            this.Graph.Size = new System.Drawing.Size(492, 342);
+            this.Graph.TabIndex = 0;
+            this.Graph.Text = "chart1";
+            // 
+            // Lagranshz
+            // 
+            this.Lagranshz.Location = new System.Drawing.Point(667, 139);
+            this.Lagranshz.Name = "Lagranshz";
+            this.Lagranshz.Size = new System.Drawing.Size(162, 74);
+            this.Lagranshz.TabIndex = 1;
+            this.Lagranshz.Text = "Формула Лагранжа";
+            this.Lagranshz.UseVisualStyleBackColor = true;
+            this.Lagranshz.Click += new System.EventHandler(this.Lagranshz_Click);
+            // 
+            // xTextBox
+            // 
+            this.xTextBox.Location = new System.Drawing.Point(656, 387);
+            this.xTextBox.Name = "xTextBox";
+            this.xTextBox.Size = new System.Drawing.Size(210, 22);
+            this.xTextBox.TabIndex = 2;
+            // 
+            // yTextBox
+            // 
+            this.yTextBox.Location = new System.Drawing.Point(656, 462);
+            this.yTextBox.Name = "yTextBox";
+            this.yTextBox.Size = new System.Drawing.Size(210, 22);
+            this.yTextBox.TabIndex = 3;
+            // 
+            // Neuton
+            // 
+            this.Neuton.Location = new System.Drawing.Point(667, 246);
+            this.Neuton.Name = "Neuton";
+            this.Neuton.Size = new System.Drawing.Size(162, 74);
+            this.Neuton.TabIndex = 4;
+            this.Neuton.Text = "Формула Ньютона";
+            this.Neuton.UseVisualStyleBackColor = true;
+            this.Neuton.Click += new System.EventHandler(this.Neuton_Click);
+            // 
+            // Save_Button
+            // 
+            this.Save_Button.Location = new System.Drawing.Point(667, 31);
+            this.Save_Button.Name = "Save_Button";
+            this.Save_Button.Size = new System.Drawing.Size(162, 74);
+            this.Save_Button.TabIndex = 5;
+            this.Save_Button.Text = "Сохранение данных";
+            this.Save_Button.UseVisualStyleBackColor = true;
+            this.Save_Button.Click += new System.EventHandler(this.Save_Button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(985, 534);
+            this.Controls.Add(this.Save_Button);
+            this.Controls.Add(this.Neuton);
+            this.Controls.Add(this.yTextBox);
+            this.Controls.Add(this.xTextBox);
+            this.Controls.Add(this.Lagranshz);
+            this.Controls.Add(this.Graph);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Graph)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart Graph;
+        private System.Windows.Forms.Button Lagranshz;
+        private System.Windows.Forms.TextBox xTextBox;
+        private System.Windows.Forms.TextBox yTextBox;
+        private System.Windows.Forms.Button Neuton;
+        private System.Windows.Forms.Button Save_Button;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Lab3VIchmat
+{
+    public partial class Form1 : Form
+    {
+        private double[] xValues;
+        private double[] yValues;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Set_Settings()
+        {
+
+        }
+
+        //Формула Лагранжа
+        private void Lagranshz_Click(object sender, EventArgs e)
+        {
+            try
+            {
+              
+                xValues = xTextBox.Text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(double.Parse).ToArray();
+                yValues = yTextBox.Text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(double.Parse).ToArray();
+
+                if (xValues.Length != yValues.Length)
+                {
+                    MessageBox.Show("Количество значений x и y должно совпадать.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                double minX = xValues.Min();
+                double maxX = xValues.Max();
+
+                double rangeX = maxX - minX;
+                minX -= rangeX * 0.05; 
+                maxX += rangeX * 0.05;
+
+                int points_amount = 100;
+         
+                double step = (maxX - minX) / points_amount; 
+
+                PointF[] points = new PointF[points_amount + 1];
+                for (int i = 0; i <= points_amount; i++)
+                {
+                    double x = minX + i * step;
+                    double y = Interpolate(xValues, yValues, x);
+                    points[i] = new PointF((float)x, (float)y);
+                }
+
+         
+                Graph.Series[1].Points.Clear();
+                Graph.Series[1].Points.DataBind(points, "X", "Y", "");
+
+                // Рисуем исходные точки
+                Graph.Series[0].Points.Clear();
+                for (int i = 0; i < xValues.Length; i++)
+                {
+                        Graph.Series[0].Points.AddXY(xValues[i], yValues[i]);
+                }
+
+                Graph.ChartAreas[0].AxisX.Minimum = minX;
+                Graph.ChartAreas[0].AxisX.Maximum = maxX; 
+                Graph.ChartAreas[0].AxisX.LabelStyle.Angle = 0;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+ 
+    private double Interpolate(double[] x, double[] y, double xEval)
+    {
+        double result = 0.0;
+        int n = x.Length;
+
+        for (int i = 0; i < n; i++)
+        {
+            double term = y[i];
+            for (int j = 0; j < n; j++)
+            {
+                if (i != j)
+                {
+                    term *= (xEval - x[j]) / (x[i] - x[j]);
+                }
+            }
+            result += term;
+        }
+        return result;
+    }
+
+        private void Save_Button_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Neuton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
